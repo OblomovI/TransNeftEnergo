2f140f5a7a5bbe1b7be6a602e4b87b1ef7ac3cde
OblomovI <ilyaoblomov2021@gmail.com>
2021-08-11 19:17:11 +0300

    Исправлено удаление из БД

--- TransNeftEnergo/Mock/MockDb.cs
+++ TransNeftEnergo/Mock/MockDb.cs
@@ -10,6 +10,9 @@ namespace TransNeftEnergo
             {
                 var AllOrganization = context.Organizations;
                 context.Organizations.RemoveRange(AllOrganization);
+                context.CurrentTransformers.RemoveRange(context.CurrentTransformers);
+                context.VoltageTransformers.RemoveRange(context.VoltageTransformers);
+                context.CurrentMeters.RemoveRange(context.CurrentMeters);
                 context.SaveChanges();
 
                 context.Organizations.Add(new Organization().Initialize() as Organization);
