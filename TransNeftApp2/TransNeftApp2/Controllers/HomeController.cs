55950b733e1451d20c633a5ba9b8c801fa71ec13
OblomovI <ilyaoblomov2021@gmail.com>
2021-08-12 11:28:25 +0300

    AddMeasuringPoint -- начало

--- TransNeftApp2/TransNeftApp2/Controllers/HomeController.cs
+++ TransNeftApp2/TransNeftApp2/Controllers/HomeController.cs
@@ -1,14 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using TransNeftApp2.Models;
+using TransNeftEnergo.Models;
+using TransNeftEnergo.DTO;
 
 namespace TransNeftApp2.Controllers
 {
@@ -21,9 +21,11 @@ namespace TransNeftApp2.Controllers
             _logger = logger;
         }
 
-        public IActionResult AddMeasuringPoint()
+        public async Task<IActionResult> AddMeasuringPoint()
         {
-            var currentMeters = await GetListFromApi<>
+            ViewBag.currentMeters = await GetListFromApi<IdNumber>(@"http://10.191.99.145/api/CurrentMeters");
+            ViewBag.currentTransformers = await GetListFromApi<IdNumber>(@"http://10.191.99.145/api/CurrentTransformers");
+            ViewBag.voltageTransformers = await GetListFromApi<IdNumber>(@"http://10.191.99.145/api/VoltageTransformers");
             return View();
         }
 
--- TransNeftApp2/TransNeftApp2/Models/IdNumber.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TransNeftApp2.Models
-{
-    public class IdNumber
-    {
-        public int Id { get; set; }
-        public int Number { get; set; }
-    }
-}
